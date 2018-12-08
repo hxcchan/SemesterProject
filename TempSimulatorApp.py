49749e0f79e1272bd3673029c9808ff8fed40c30
Song Han <35266522+hxcchan@users.noreply.github.com>
2018-12-07 20:23:12 -0500

    SemesterProject

---
+++ TempSimulatorApp.py
@@ -0,0 +1,13 @@
+'''
+Created on 2018
+
+@author: HSong
+'''
+
+#This is a module that will pass data from sensors and start running the app.
+
+from SemesterProject import SensorReader
+
+startRunning = senseHatAdaptor.start()
+
+startRunning.run()
